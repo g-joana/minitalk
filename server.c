6226b38107e4f7645afbb9d992e42bb90aa7a88e
jgils <gilsjoana@gmail.com>
2024-04-10 15:59:16 -0300

    minitalk all done

--- server.c
+++ server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgils <jgils@student.42.rio>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 15:47:04 by jgils             #+#    #+#             */
+/*   Updated: 2024/04/10 15:56:09 by jgils            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <signal.h>
 #include "libft/libft.h"
@@ -7,7 +19,7 @@ void	handle_bit(int signal)
 	static char	byte = '\0';
 	static int	count = 7;
 
-	if(signal == SIGUSR1)
+	if (signal == SIGUSR1)
 	{
 		byte = byte | (1 << count--);
 	}
@@ -28,6 +40,7 @@ int	main(void)
 	ft_printf("%i\n", getpid());
 	signal(SIGUSR1, handle_bit);
 	signal(SIGUSR2, handle_bit);
-	while (1);
+	while (1)
+		;
 	return (0);
 }
